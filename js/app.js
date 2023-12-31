512403a5f368f411d28e9374ffee2e0b89635046
mahboubeh shoubi <mahboobeh.shobi.65@gmail.com>
2023-12-31 09:11:40 +0330

    create functions changeComplete and editedHandler and editTodoList for Handlered todoes in todo List in app.js and insert checkBox input to index.html and a key to flag name to todo Object in app.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
+const flaggedCheckedBox = document.getElementById("flagged");
+const modeTodo = document.getElementById("select-status");
+const subjectTodo = document.getElementById("subject-todo");
 const dateTodo = document.getElementById("date-todo");
 const timeTodo = document.getElementById("time-todo");
 const textTaskTodo = document.getElementById("text-task-todo");
 const addButton = document.getElementById("add-button");
-const modeTodo = document.getElementById("select-status");
-const subjectTodo = document.getElementById("subject-todo");
+const editButton = document.getElementById("edit-button");
 
 
 const filters = document.querySelectorAll(".filter");
@@ -12,11 +14,20 @@ const mainSection = document.getElementById("main");
 const loading = document.getElementById("loading");
 const addTaskTodo = document.getElementById("add-task-todo")
 
+const todayCount = document.querySelector(".today-counter");
+const allCount = document.querySelector(".all-counter");
+
+
+
+const dayWeek = ["ُSunday" , "Monday" , "Tuesday" , "wednesday" , "Thursday" , "Friday" , "Saturday"];
+const mount = ["January" , "February" , "March" , "April" , "May" , "June" , "July" , "August" , "September" , "October" , "November" , "December"];
+
+
 
 //---------------------
-const date = new Date();
-const day = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
-console.log(day);
+const fullDate = new Date();
+const date = `${fullDate.getFullYear()}-${fullDate.getMonth() + 1}-${fullDate.getDate()}`;
+console.log(date);
 
 //Todos----------------------------------------------
 let todos = JSON.parse(localStorage.getItem("todos")) || [];
@@ -35,22 +46,87 @@ const generatedId = () => {
 
 
 
-//Display Todos------------------------------------------------
-const displayTodo = () => {
+//-change complete in Todo ----------------------------------------------
+const changeComplete = (id) => {
+    const changStatus = todos.find(todo => todo.id === id);
+    changStatus.complete = !changStatus.complete;
+    saveLocalstorage();
+    displayTodo(todos);
+}
 
-    todos.forEach(todo => {
-        loading.innerHTML = `
-              <div class="todo-box">
-                  <h3>${todo.day === day.toString() ? "Today" : `${todo.day} : ${todo.day}` }</h3>
-                  <div class="box">
-                      <input type="radio" name="complete">
-                      <div>
-                        <span>${todo.mode} and ${todo.subject} | </span>
-                        <span>${todo.day} | </span>
-                        <span>${todo.time}</span>
-                        <p>${todo.task}</p>
-                      </div>
-                  </div>
+
+//Edit todo in list handler --------------------------------------------
+const editedHandler = (id) => {
+  const filterTodo = todos.find(todo => todo.id === id);
+  modeTodo.value = filterTodo.mode;
+  subjectTodo.value= filterTodo.subject;
+  dateTodo.value= filterTodo.day;
+  timeTodo.value= filterTodo.time;
+  textTaskTodo.value=filterTodo.task;
+
+ addButton.style.display="none";
+ editButton.style.display="block";
+ editButton.dataset.id=id;
+}
+
+const editTodoList = (event) => {
+    const idTodo = event.target.dataset.id;
+    console.log(idTodo);
+    const findTodo = todos.find(todo => todo.id === idTodo);
+    findTodo.mode = modeTodo.value;
+    findTodo.subject = subjectTodo.value;
+    findTodo.day = dateTodo.value;
+    findTodo.time = timeTodo.value;
+    findTodo.task = textTaskTodo.value;
+    saveLocalstorage();
+    displayTodo(todos);
+    editButton.style.display="none";
+    addButton.style.display="block";
+
+    modeTodo.value = "";
+    subjectTodo.value="";
+    dateTodo.value="";
+    timeTodo.value="";
+    textTaskTodo.value="";
+}
+
+
+//Delete Todo for list Todos--------------------------------------
+const deleteTodo = (id) => {
+    const newTodos = todos.filter(todo => todo.id !== id);
+    todos = newTodos;
+    saveLocalstorage();
+    displayTodo(todos);
+}
+
+
+
+//Display Todos------------------------------------------------
+const displayTodo = (todoList) => {
+    loading.innerHTML="";
+
+    if (!todoList.length){
+       loading.innerHTML = `<div> <p>Note found Todo List Data</p> </div>`
+    }
+
+    todoList.forEach(todo => {
+        const splitTodoDay = todo.day.split("-");
+
+        loading.innerHTML += `
+              <div class=${todo.complete ? "todo-boxes-complete" : "todo-boxes"} >
+                 <div class="info-todo">
+                    <h3>${todo.day === date.toString() ? "Today" : `${splitTodoDay[0]}  ${mount[+splitTodoDay[1] -1]}  ${+splitTodoDay[2]}` }</h3>
+                    <div class="box">
+                       <p>${todo.mode} <span>and</span>  ${todo.subject} | <span>${todo.time}</span></p>
+                       <p>${todo.task}</p>
+                    </div>
+                 </div>
+                 <div class="control-box">
+                   <button class="edit-btn" onclick="editedHandler('${todo.id}')" >Edit</button>
+                   <button class="complete-btn" onclick="changeComplete('${todo.id}')">Complete</button>  
+                   <button class="delete-todo-btn" onclick="deleteTodo('${todo.id}')">Delete</button>           
+                   <img id="tick-marked" src="../image/icons/tick-mark.svg">
+                 </div>
               </div>
        `
     })
@@ -61,11 +137,20 @@ const displayTodo = () => {
 //Add Todos-------------------------------------------------------------
 const createAddTodoHandler = () => {
 
+
+
     const subject = subjectTodo.value;
     const day = dateTodo.value;
     const time = timeTodo.value;
     const task = textTaskTodo.value;
     const mode = modeTodo.value;
+    let flag = null;
+
+    if(flaggedCheckedBox.checked){
+        flag = true;
+    }else {
+        flag = false;
+    }
 
     const todo = {
         id : generatedId(),
@@ -75,21 +160,38 @@ const createAddTodoHandler = () => {
         day ,
         time,
         task,
+        flag ,
     }
 
-    todos.push(todo);
-    saveLocalstorage();
-    displayTodo();
-    modeTodo.value="";
-    subjectTodo.value="";
-    dateTodo.value="";
-    timeTodo.value="";
-    textTaskTodo.value="";
+    if (subject && day && time && task && mode){
+        todos.push(todo);
+        saveLocalstorage();
+        displayTodo(todos);
+        modeTodo.value="";
+        subjectTodo.value="";
+        dateTodo.value="";
+        timeTodo.value="";
+        textTaskTodo.value="";
+        flaggedCheckedBox.checked=false;
+    }else {
+        alert("please Enter informations");
+    }
+
+
 }
 
+//windows loading-------------------------------
+const firstLoading = () => {
+    const today = todos.filter(todo => todo.day === date.toString());
+    todayCount.innerText = today.length;
+    allCount.innerText= todos.length;
+
+    }
 
 
+window.addEventListener("load" , firstLoading);
 addButton.addEventListener("click" , createAddTodoHandler);
+editButton.addEventListener("click" , editTodoList);
 addTodo.addEventListener("click" , () => {
     addTaskTodo.style.display="inline-block";
 })
