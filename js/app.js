d6b030d681b17f421ae1a4515eb461f4e7f0f67c
mahboubeh shoubi <mahboobeh.shobi.65@gmail.com>
2023-12-31 22:17:03 +0330

    create event for All TodoList and pendding and completed Todos

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+const mainSection = document.getElementById("main");
 const flaggedCheckedBox = document.getElementById("flagged");
 const modeTodo = document.getElementById("select-status");
 const subjectTodo = document.getElementById("subject-todo");
@@ -7,15 +8,18 @@ const textTaskTodo = document.getElementById("text-task-todo");
 const addButton = document.getElementById("add-button");
 const editButton = document.getElementById("edit-button");
 
+const navIcon = document.getElementById("nav-bar-icon");
+const imaBackgroundList = document.querySelectorAll(".bg-img");
+const boxBackground = document.getElementById("box-background");
 
-const filters = document.querySelectorAll(".filter");
 const addTodo = document.getElementById("add-todo");
-const mainSection = document.getElementById("main");
 const loading = document.getElementById("loading");
 const addTaskTodo = document.getElementById("add-task-todo")
+const filters = document.querySelectorAll(".filter");
 
 const todayCount = document.querySelector(".today-counter");
 const allCount = document.querySelector(".all-counter");
+const flagCount = document.querySelector(".flagged-counter");
 
 
 
@@ -42,6 +46,11 @@ const saveLocalstorage = () => {
 //generatedId--------------------------------------------
 const generatedId = () => {
     return Math.round(Math.random() * Math.random() * Math.pow(10 , 15)).toString();
+}
+
+
+const showBackgroundHandler = () => {
+    boxBackground.style.display="block";
 }
 
 
@@ -115,18 +124,26 @@ const displayTodo = (todoList) => {
         loading.innerHTML += `
               <div class=${todo.complete ? "todo-boxes-complete" : "todo-boxes"} >
                  <div class="info-todo">
-                    <h3>${todo.day === date.toString() ? "Today" : `${splitTodoDay[0]}  ${mount[+splitTodoDay[1] -1]}  ${+splitTodoDay[2]}` }</h3>
+                    <h3>${todo.day === date.toString() ? "Today" : `${splitTodoDay[0]}  ${mount[+splitTodoDay[1] -1]}  ${+splitTodoDay[2]}`} 
+                         ${todo.flag === true && `<img class="favourite-icons" src="../image/icons/favourite.svg"/> 
+                            <img class="favourite-icons" src="../image/icons/favourite.svg"/> 
+                            <img class="favourite-icons" src="../image/icons/favourite.svg"/>`} </h3>
                     <div class="box">
                        <p>${todo.mode} <span>and</span>  ${todo.subject} | <span>${todo.time}</span></p>
                        <p>${todo.task}</p>
                     </div>
                  </div>
+                 
                  <div class="control-box">
                    <button class="edit-btn" onclick="editedHandler('${todo.id}')" >Edit</button>
                    <button class="complete-btn" onclick="changeComplete('${todo.id}')">Complete</button>  
-                   <button class="delete-todo-btn" onclick="deleteTodo('${todo.id}')">Delete</button>           
-                   <img id="tick-marked" src="../image/icons/tick-mark.svg">
+                   <button class="delete-todo-btn" onclick="deleteTodo('${todo.id}')">Delete</button>
                  </div>
+                 <div class="img-control">
+                   <img id="tick-marked" src="../image/icons/tick-mark.svg">
+                   <img id="delete-todo-complete" src="../image/icons/close.svg" onclick="deleteTodo('${todo.id}')">
+                 </div>                                               
+                
               </div>
        `
     })
@@ -180,18 +197,50 @@ const createAddTodoHandler = () => {
 
 }
 
+//Filter Display-----------------------------------
+
+const filterDisplay = (event) => {
+  let filterName = event.target.dataset.filter;
+
+  switch (filterName){
+      case "completed": {
+          const completeList = todos.filter(todo => todo.complete === true);
+          displayTodo(completeList);
+          return;
+      }
+      case "pending": {
+          const pendingList = todos.filter(todo => todo.complete === false);
+          displayTodo(pendingList);
+          return;
+      }
+      default:
+          displayTodo(todos);
+          return;
+  }
+}
+
 //windows loading-------------------------------
 const firstLoading = () => {
     const today = todos.filter(todo => todo.day === date.toString());
     todayCount.innerText = today.length;
     allCount.innerText= todos.length;
 
+    const flagged = todos.filter(todo => todo.flag === true);
+    flagCount.innerText = flagged.length;
+
+    displayTodo(todos);
+
+
     }
 
 
 window.addEventListener("load" , firstLoading);
+navIcon.addEventListener("click" , showBackgroundHandler);
 addButton.addEventListener("click" , createAddTodoHandler);
 editButton.addEventListener("click" , editTodoList);
 addTodo.addEventListener("click" , () => {
     addTaskTodo.style.display="inline-block";
+})
+filters.forEach(filter => {
+    filter.addEventListener("click" , filterDisplay)
 })
